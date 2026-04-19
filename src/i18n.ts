d6e7970cab7cdcdfb1172331e74023f6f245dda6
Bernadette König <b.koenig0303@gmail.com>
2026-04-19 13:42:37 +0200

    feat: Update team role titles
    Rename 'Executive Director' and 'Cultural Program Manager' to 'Founder' to better reflect the initial vision and leadership of the center.

--- src/i18n.ts
+++ src/i18n.ts
@@ -117,8 +117,8 @@ const resources = {
           title: 'Our Team',
           quote: '"Dedicated professionals committed to the preservation and promotion of Mongolian heritage in Europe."',
           roles: {
-            director: 'Executive Director',
-            manager: 'Cultural Program Manager',
+            director: 'Founder',
+            manager: 'Founder',
             outreach: 'Community Outreach'
           }
         },
